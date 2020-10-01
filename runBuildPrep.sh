0ac5bbfcb53630876d4e96561aae9f24c6ca26ea
Alex Lovell-Troy <alovelltr@cray.com>
2020-10-01 20:52:26 +0300

    Trying the loftman method

--- runBuildPrep.sh
+++ runBuildPrep.sh
@@ -1,3 +1,19 @@
-#!/bin/bash -x
+set -e
 
-zypper --plus-repo=http://dst.us.cray.com/dstrepo/bloblets/os/dev/mirrors/rpms/sles/15sp2/ --no-gpg-checks in -y --auto-agree-with-licenses go1.14
+if [[ "$1" != "binary-only" ]]; then
+    GO_VERSION="1.14.9"
+    INSTALLED_GO_VERSION=$(go version | awk '{print $3}')
+
+    if [[ "go${GO_VERSION}" !=  $INSTALLED_GO_VERSION ]]; then
+        echo "Upgrading go from version ${INSTALLED_GO_VERSION} to ${GO_VERSION}"
+        go get golang.org/dl/go$GO_VERSION || true
+        $GOPATH/bin/go$GO_VERSION download || true
+        GO_EXEC=$(which go)
+        rm -f $GO_EXEC
+        cp $GOPATH/bin/go$GO_VERSION $GO_EXEC
+    fi
+
+    mkdir -p $GOPATH/bin
+    mkdir -p $GOPATH/src
+    mkdir -p $GOPATH/pkg
+fi
