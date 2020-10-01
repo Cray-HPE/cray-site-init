c9407f03c64b774dae4d0554018cfb89527293b0
Alex Lovell-Troy <alovelltr@cray.com>
2020-10-01 18:24:26 +0300

    Adding runBuildPrep to get a more modern golang

--- runBuildPrep.sh
+++ runBuildPrep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 
-zypper --plus-repo=http://dst.us.cray.com/dstrepo/shasta-cd-repo/bloblets/os/dev/mirrors/rpms/sles/15sp2-all/ in -y --auto-agree-with-licenses go1.14
+zypper --plus-repo=http://dst.us.cray.com/dstrepo/bloblets/os/dev/mirrors/rpms/sles/15sp2/ --no-gpg-checks in -y --auto-agree-with-licenses go1.14
