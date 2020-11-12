50a7a79db65611bc0b7f9757a10532550df9dedf
Alex Lovell-Troy <alovelltr@cray.com>
2020-11-12 16:09:02 +0200

    Fixing lint issue

--- pkg/shasta/basecamp.go
+++ pkg/shasta/basecamp.go
@@ -27,33 +27,33 @@ type CloudInit struct {
 type BaseCampGlobals struct {
 
 	// CEPH Installation Globals
-	CephFSImage          string `json:ceph-cephfs-image`
-	CephRBDImage         string `json:ceph-rbd-image`
-	CephStorageNodeCount string `json:ceph-num-storage-nodes` // "3"
-	CephRGWVip           string `json:rgw-virtual-ip`
-	CephWipe             bool   `json:wipe-ceph-osds`
+	CephFSImage          string `json:"ceph-cephfs-image"`
+	CephRBDImage         string `json:"ceph-rbd-image"`
+	CephStorageNodeCount string `json:"ceph-num-storage-nodes"` // "3"
+	CephRGWVip           string `json:"rgw-virtual-ip"`
+	CephWipe             bool   `json:"wipe-ceph-osds"`
 
 	// Not sure what consumes these.
 	// Are we sure we want to reference something outside the cluster for either of them?
-	ChartRepo     string `json:chart-repo`            // "http://helmrepo.dev.cray.com:8080"
-	ImageRegistry string `json:docker-image-registry` // dtr.dev.cray.com"
+	ChartRepo     string `json:"chart-repo"`            // "http://helmrepo.dev.cray.com:8080"
+	ImageRegistry string `json:"docker-image-registry"` // dtr.dev.cray.com"
 
 	// Commenting out several that I think we don't need
 	// Domain string `json:domain`        // dnsmasq should provide this
 	// DNSServer string `json:dns-server` // dnsmasq should provide this
 	// CanGateway  string `json:can-gw`   // dnsmasq should provide this
-	CanInterface string `json:can-if` // Do we need this?
+	CanInterface string `json:"can-if"` // Do we need this?
 
 	// Kubernetes Installation Globals
-	KubernetesFirstMasterHostname string `json:first-master-hostname` // What's this for?
-	KubernetesVIP                 string `json:kubernetes-virtual-ip`
-	KubernetesMaxPods             string `json:kubernetes-max-pods-per-node`
-	KubernetesPodCIDR             string `json:kubernetes-pods-cidr`     // "10.32.0.0/12"
-	KubernetesServicesCIDR        string `json:kubernetes-services-cidr` // "10.16.0.0/12"
-	KubernetesWeaveMTU            string `json:kubernetes-weave-mtu`     // 1460
+	KubernetesFirstMasterHostname string `json:"first-master-hostname"` // What's this for?
+	KubernetesVIP                 string `json:"kubernetes-virtual-ip"`
+	KubernetesMaxPods             string `json:"kubernetes-max-pods-per-node"`
+	KubernetesPodCIDR             string `json:"kubernetes-pods-cidr"`     // "10.32.0.0/12"
+	KubernetesServicesCIDR        string `json:"kubernetes-services-cidr"` // "10.16.0.0/12"
+	KubernetesWeaveMTU            string `json:"kubernetes-weave-mtu"`     // 1460
 
 	// NTP Setup Globals
-	NTPPeers    string `json:ntp-peers`
-	NTPAllow    string `json:ntp_local_nets`
-	NTPUpstream string `json:ntp-upstream-server`
+	NTPPeers    string `json:"ntp-peers"`
+	NTPAllow    string `json:"ntp_local_nets"`
+	NTPUpstream string `json:"ntp-upstream-server"`
 }
