290d552cd4e2dcf6db84a1d6a87bcd99d73f94fd
Jacob Salmela <jacob.salmela@hpe.com>
2021-07-08 14:57:38 -0500

    MTL-1397 convert monolithic function into unit-testable functions
    Signed-off-by: Jacob Salmela <jacob.salmela@hpe.com>

--- cmd/manage_input_files.go
+++ cmd/manage_input_files.go
@@ -9,6 +9,7 @@ import (
 	"log"
 
 	"github.com/spf13/viper"
+	"path/filepath"
 	shcd_parser "stash.us.cray.com/HMS/hms-shcd-parser/pkg/shcd-parser"
 	"stash.us.cray.com/MTL/csi/internal/files"
 	"stash.us.cray.com/MTL/csi/pkg/csi"
@@ -19,43 +20,18 @@ type cabinetDefinition struct {
 	startingID int
 }
 
-func collectInput(v *viper.Viper) ([]shcd_parser.HMNRow, []*csi.LogicalNCN, []*csi.ManagementSwitch, csi.SLSGeneratorApplicationNodeConfig, []csi.CabinetGroupDetail) {
-	// The installation requires a set of information in order to proceed
-	// First, we need some kind of representation of the physical hardware
-	// That is generally represented through the hmn_connections.json file
-	// which is literally a cabling map with metadata about the NCNs and
-	// River Compute node BMCs, Columbia Rosetta Switches, and PDUs.
-	//
-	// From the hmn_connections file, we can create a set of HMNRow objects
-	// to use for populating SLS.
-	hmnRows, err := loadHMNConnectionsFile(v.GetString("hmn-connections"))
+func collectHMNRows(v *viper.Viper) []shcd_parser.HMNRow {
+	seedFileHmnConnections := filepath.Dir(viper.ConfigFileUsed()) + "/" + v.GetString("hmn-connections")
+	hmnRows, err := loadHMNConnectionsFile(seedFileHmnConnections)
 	if err != nil {
 		log.Fatalf("unable to load hmn connections, %v \n", err)
 	}
+	return hmnRows
+}
 
-	// SLS also needs to know about our networking configuration.  In order to do that,
-	// we need to load the switches
-	switches, err := csi.ReadSwitchCSV(v.GetString("switch-metadata"))
-	if err != nil {
-		log.Fatalln("Couldn't extract switches", err)
-	}
-
-	// Normalize the management switch data, before validation
-	for _, mySwitch := range switches {
-		mySwitch.Normalize()
-	}
-
-	if err := validateSwitchInput(switches); err != nil {
-		log.Println("Unable to get reasonable Switches from your csv")
-		log.Println("Does your header match the preferred style? Switch Xname,Type,Brand")
-		log.Fatal("CSV Parsing failed.  Can't continue.")
-	}
-
-	// This is techincally sufficient to generate an SLSState object, but to do so now
-	// would not include extended information about the NCNs and Network Switches.
-	//
-	// The first step in building the NCN map is to read the NCN Metadata file
-	ncns, err := csi.ReadNodeCSV(v.GetString("ncn-metadata"))
+func collectNCNMeta(v *viper.Viper) []*csi.LogicalNCN {
+	seedFileNcnMetadata := filepath.Dir(viper.ConfigFileUsed()) + "/" + v.GetString("ncn-metadata")
+	ncns, err := csi.ReadNodeCSV(seedFileNcnMetadata)
 	if err != nil {
 		log.Fatalln("Couldn't extract ncns", err)
 	}
@@ -71,8 +47,57 @@ func collectInput(v *viper.Viper) ([]shcd_parser.HMNRow, []*csi.LogicalNCN, []*c
 		log.Fatal("CSV Parsing failed.  Can't continue.")
 	}
 
-	// Cabinet Map Configuration
-	// This is an optional input file
+	return ncns
+}
+
+func collectSwitches(v *viper.Viper) []*csi.ManagementSwitch {
+	seedFileSwitchMetadata := filepath.Dir(v.ConfigFileUsed()) + "/" + v.GetString("switch-metadata")
+
+	switches, err := csi.ReadSwitchCSV(seedFileSwitchMetadata)
+	if err != nil {
+		log.Fatalln("Couldn't extract switches", err)
+	}
+
+	// Normalize the management switch data, before validation
+	for _, mySwitch := range switches {
+		mySwitch.Normalize()
+	}
+
+	if err := validateSwitchInput(switches); err != nil {
+		log.Println("Unable to get reasonable Switches from your csv")
+		log.Println("Does your header match the preferred style? Switch Xname,Type,Brand")
+		log.Fatal("CSV Parsing failed.  Can't continue.")
+	}
+
+	return switches
+}
+
+func collectApplicationNodeConfig(v *viper.Viper) csi.SLSGeneratorApplicationNodeConfig {
+	var applicationNodeConfig csi.SLSGeneratorApplicationNodeConfig
+	if v.IsSet("application-node-config-yaml") {
+		seedFileAppNodeConfig := filepath.Dir(viper.ConfigFileUsed()) + "/" + v.GetString("application-node-config-yaml")
+
+		log.Printf("Using application node config: %s\n", seedFileAppNodeConfig)
+		err := files.ReadYAMLConfig(seedFileAppNodeConfig, &applicationNodeConfig)
+		if err != nil {
+			log.Fatalf("Unable to parse application-node-config file: %s\nError: %v", seedFileAppNodeConfig, err)
+		}
+	}
+
+	// Normalize application node config
+	if err := applicationNodeConfig.Normalize(); err != nil {
+		log.Fatalf("Failed to normalize application node config. Error: %s", err)
+	}
+
+	// Validate Application node config
+	if err := applicationNodeConfig.Validate(); err != nil {
+		log.Fatalf("Failed to validate application node config. Error: %s", err)
+	}
+
+	return applicationNodeConfig
+}
+
+func collectCabinets(v *viper.Viper) []csi.CabinetGroupDetail {
 	var cabDetailFile csi.CabinetDetailFile
 	if v.IsSet("cabinets-yaml") {
 		err := files.ReadYAMLConfig(v.GetString("cabinets-yaml"), &cabDetailFile)
@@ -89,28 +114,38 @@ func collectInput(v *viper.Viper) ([]shcd_parser.HMNRow, []*csi.LogicalNCN, []*c
 	}
 	cabinetDetailList := buildCabinetDetails(cabDefinitions, cabDetailFile)
 
+	return cabinetDetailList
+}
+
+func collectInput(v *viper.Viper) ([]shcd_parser.HMNRow, []*csi.LogicalNCN, []*csi.ManagementSwitch, csi.SLSGeneratorApplicationNodeConfig, []csi.CabinetGroupDetail) {
+	// The installation requires a set of information in order to proceed
+	// First, we need some kind of representation of the physical hardware
+	// That is generally represented through the hmn_connections.json file
+	// which is literally a cabling map with metadata about the NCNs and
+	// River Compute node BMCs, Columbia Rosetta Switches, and PDUs.
+	//
+	// From the hmn_connections file, we can create a set of HMNRow objects
+	// to use for populating SLS.
+	// this seedfile should be in the same place as the config, so use that to craft the path
+	hmnRows := collectHMNRows(v)
+
+	// This is techincally sufficient to generate an SLSState object, but to do so now
+	// would not include extended information about the NCNs and Network Switches.
+	//
+	// The first step in building the NCN map is to read the NCN Metadata file
+	ncns := collectNCNMeta(v)
+
+	// SLS also needs to know about our networking configuration.  In order to do that,
+	// we need to load the switches
+	switches := collectSwitches(v)
+
 	// Application Node configration for SLS Config Generator
 	// This is an optional input file
-	var applicationNodeConfig csi.SLSGeneratorApplicationNodeConfig
-	if v.IsSet("application-node-config-yaml") {
-		applicationNodeConfigPath := v.GetString("application-node-config-yaml")
+	applicationNodeConfig := collectApplicationNodeConfig(v)
 
-		log.Printf("Using application node config: %s\n", applicationNodeConfigPath)
-		err := files.ReadYAMLConfig(applicationNodeConfigPath, &applicationNodeConfig)
-		if err != nil {
-			log.Fatalf("Unable to parse application-node-config file: %s\nError: %v", applicationNodeConfigPath, err)
-		}
-	}
-
-	// Normalize application node config
-	if err := applicationNodeConfig.Normalize(); err != nil {
-		log.Fatalf("Failed to normalize application node config. Error: %s", err)
-	}
-
-	// Validate Application node config
-	if err := applicationNodeConfig.Validate(); err != nil {
-		log.Fatalf("Failed to validate application node config. Error: %s", err)
-	}
+	// Cabinet Map Configuration
+	// This is an optional input file
+	cabinetDetailList := collectCabinets(v)
 
 	return hmnRows, ncns, switches, applicationNodeConfig, cabinetDetailList
 }
