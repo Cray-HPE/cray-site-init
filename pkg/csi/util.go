9120e4f92379e005171e50a3612cf8c9b85bdd05
Jacob Salmela <jacob.salmela@hpe.com>
2021-07-16 08:19:11 -0500

    MTL-1340 add an integration test for config init
    Signed-off-by: Jacob Salmela <jacob.salmela@hpe.com>

--- pkg/csi/util.go
+++ pkg/csi/util.go
@@ -4,6 +4,12 @@ Copyright 2021 Hewlett Packard Enterprise Development LP
 
 package csi
 
+import (
+	"bytes"
+
+	"github.com/spf13/cobra"
+)
+
 // stringInSlice is shorthand
 func stringInSlice(a string, list []string) bool {
 	for _, b := range list {
@@ -12,4 +18,16 @@ func stringInSlice(a string, list []string) bool {
 		}
 	}
 	return false
+}
+
+// ExecuteCommandC runs a cobra command
+func ExecuteCommandC(root *cobra.Command, args []string) (c *cobra.Command, output string, err error) {
+	buf := new(bytes.Buffer)
+	root.SetOut(buf)
+	root.SetErr(buf)
+	root.SetArgs(args)
+
+	c, err = root.ExecuteC()
+
+	return c, buf.String(), err
 }
