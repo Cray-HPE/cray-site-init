596bc45fc10fe6cb64e101cd686a544e69464c3d
Sean Lynn <sean.lynn@hpe.com>
2021-12-06 16:16:07 +0000

    Remove unused SupernetRouter field from subnets.

--- pkg/csi/network.go
+++ pkg/csi/network.go
@@ -37,7 +37,6 @@ type IPV4Subnet struct {
 	VlanID           int16           `yaml:"vlan_id" form:"vlan_id" mapstructure:"vlan_id"`
 	Comment          string          `yaml:"comment"`
 	Gateway          net.IP          `yaml:"gateway"`
-	SupernetRouter   net.IP          `yaml:"_"`
 	PITServer        net.IP          `yaml:"_"`
 	DNSServer        net.IP          `yaml:"dns_server"`
 	DHCPStart        net.IP          `yaml:"iprange-start"`
