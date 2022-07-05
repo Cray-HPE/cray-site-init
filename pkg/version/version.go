d4fd590a9640352b6ea9c85cb0bad69cccd60752
Russell Bunch <doomslayer@hpe.com>
2022-07-05 13:22:14 -0500

    MTL-1836 Remove App Version (#202)
    App Version reports '..' as its version since the removal of the
    .version file.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,25 +3,21 @@ package version
 import (
 	"fmt"
 	"runtime"
-	"strings"
 )
 
 // Info is heavily informed by the Kubernetes Versioning System.
 type Info struct {
-	Major      string `json:"major"`
-	Minor      string `json:"minor"`
-	FixVr      string `json:"fixvr"`
-	GitVersion string `json:"gitVersion"`
-	GitCommit  string `json:"gitCommit"`
-	BuildDate  string `json:"buildDate"`
-	GoVersion  string `json:"goVersion"`
-	Compiler   string `json:"compiler"`
-	Platform   string `json:"platform"`
+	Version   string `json:"version"`
+	GitCommit string `json:"gitCommit"`
+	BuildDate string `json:"buildDate"`
+	GoVersion string `json:"goVersion"`
+	Compiler  string `json:"compiler"`
+	Platform  string `json:"platform"`
 }
 
 // String returns info as a human-friendly version string.
 func (info Info) String() string {
-	return info.GitVersion
+	return info.Version
 }
 
 // Get returns the overall codebase version. It's for detecting
@@ -30,25 +26,12 @@ func Get() Info {
 	// These variables typically come from -ldflags settings and in
 	// their absence fallback to the settings in ./base.go
 
-	// If major and minor are not individually set with ldflags, we can
-	// separate the .version file as passed with an ldflag
-	if fsMajor == "" && fsMinor == "" {
-		if fsVersion != "" {
-			fsVersions := strings.Split(fsVersion, ".")
-			fsMajor = fsVersions[0]
-			fsMinor = fsVersions[1]
-			fsFixVr = fsVersions[2]
-		}
-	}
 	return Info{
-		Major:      fsMajor,
-		Minor:      fsMinor,
-		FixVr:      fsFixVr,
-		GitVersion: gitVersion,
-		GitCommit:  sha1ver,
-		BuildDate:  buildDate,
-		GoVersion:  runtime.Version(),
-		Compiler:   runtime.Compiler,
-		Platform:   fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+		Version:   version,
+		GitCommit: sha1ver,
+		BuildDate: buildDate,
+		GoVersion: runtime.Version(),
+		Compiler:  runtime.Compiler,
+		Platform:  fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
 	}
 }
