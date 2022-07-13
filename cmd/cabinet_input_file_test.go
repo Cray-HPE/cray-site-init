18cb7e3dfffb47b84a4d1ef1e562e868e0b02464
Ryan Sjostrand <77114568+rsjostrand-hpe@users.noreply.github.com>
2022-07-13 17:17:39 -0500

    MTL-1849: Fix TestCabinetDefinitionWithHill after EX2500 cabinet support added (#207)
    MTL-1849: Fix TestCabinetDefinitionWithHill after EX2500 cabinet support

--- cmd/cabinet_input_file_test.go
+++ cmd/cabinet_input_file_test.go
@@ -8,6 +8,7 @@ Copyright 2021 Hewlett Packard Enterprise Development LP
 package cmd
 
 import (
+	"encoding/json"
 	"log"
 	"testing"
 
@@ -38,6 +39,17 @@ func testProcessWithHill(t *testing.T) csi.CabinetDetailFile {
 	return cabDetailFile
 }
 
+func testFindCabinetGroupDetail(t *testing.T, cabinetDetailList []csi.CabinetGroupDetail, wantedKind csi.CabinetKind) *csi.CabinetGroupDetail {
+	for _, cabinetDetail := range cabinetDetailList {
+		if cabinetDetail.Kind == wantedKind {
+			return &cabinetDetail
+		}
+	}
+
+	t.Errorf("Failed to kind %s in cabinetDetailList", wantedKind)
+	return nil
+}
+
 func TestCabinetDefinitionWithHill(t *testing.T) {
 	cabDetailFile := testProcessWithHill(t)
 	cabDefinitions := make(map[csi.CabinetKind]cabinetDefinition)
@@ -55,8 +67,41 @@ func TestCabinetDefinitionWithHill(t *testing.T) {
 	}
 
 	cabinetDetailList := buildCabinetDetails(cabDefinitions, cabDetailFile)
-	if len(cabinetDetailList) != 3 {
+	if len(cabinetDetailList) != len(csi.ValidCabinetTypes) {
 		t.Errorf("%+v", cabinetDetailList)
 	}
-	log.Printf("%+v \n", cabinetDetailList)
+
+	// Hill
+	hill := testFindCabinetGroupDetail(t, cabinetDetailList, csi.CabinetKindHill)
+	if hill.Cabinets != 1 {
+		t.Errorf("Expected 1 hill cabinet, but got %v", hill.Cabinets)
+	}
+
+	// Mountain
+	mountain := testFindCabinetGroupDetail(t, cabinetDetailList, csi.CabinetKindMountain)
+	if mountain.Cabinets != 20 {
+		t.Errorf("Expected 20 mountain cabinets, but got %v", mountain.Cabinets)
+	}
+
+	// River
+	river := testFindCabinetGroupDetail(t, cabinetDetailList, csi.CabinetKindRiver)
+	if river.Cabinets != 1 {
+		t.Errorf("Expected 1 river cabinet, but got %v", river.Cabinets)
+	}
+
+	// The EX{20,25,30,40}00 cabinet kinds are expected to be 0
+	for _, cabinetKind := range []csi.CabinetKind{csi.CabinetKindEX2000, csi.CabinetKindEX2500, csi.CabinetKindEX3000, csi.CabinetKindEX4000} {
+		cabinetGroupDetail := testFindCabinetGroupDetail(t, cabinetDetailList, cabinetKind)
+		if cabinetGroupDetail.Cabinets != 0 {
+			t.Errorf("Expected 0 %v cabinets, but got %v", cabinetKind, cabinetGroupDetail.Cabinets)
+		}
+	}
+
+	// Make the cabinet detail list a bit more readable
+	cabinetDetailListJSON, err := json.MarshalIndent(cabinetDetailList, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("%+v \n", string(cabinetDetailListJSON))
 }
