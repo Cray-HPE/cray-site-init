747eabcb4168c14389238643cdedab6e6a46d665
Russell Bunch <doomslayer@hpe.com>
2022-09-23 20:46:42 -0500

    MTL-1695 `pit populate` is no longer used (#256)
    * MTL-1695 `pit populate` is no longer used
    
    * Update License

--- cmd/populate.go
+++ cmd/populate.go
@@ -1,8 +1,30 @@
-package cmd
-
 /*
-Copyright 2021 Hewlett Packard Enterprise Development LP
-*/
+ *
+ *  MIT License
+ *
+ *  (C) Copyright 2021-2022 Hewlett Packard Enterprise Development LP
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *  OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package cmd
 
 import (
 	"fmt"
@@ -17,6 +39,8 @@ import (
 )
 
 // populateCmd moves csi files and node images onto the usb device
+// Deprecated: The USB path no longer uses this function since it is now completely identical to the
+// RemoteISO path for configuring the PIT.
 var populateCmd = &cobra.Command{
 	Use:   "populate",
 	Short: "Populates the LiveCD with configs",
@@ -24,6 +48,7 @@ var populateCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("populate called")
 	},
+	Deprecated: "No longer used for CSM 1.3 or newer CSM installations.",
 }
 
 // copyAllFiles copies ONLY files from one spot to another
