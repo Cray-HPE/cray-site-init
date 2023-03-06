7fc3d9c62257e51879dd50ad1dad80ce265ff913
Russell Bunch <doomslayer@hpe.com>
2023-03-06 03:59:46 -0600

    Run `go fmt`

--- pkg/pit/credentials.go
+++ pkg/pit/credentials.go
@@ -24,7 +24,7 @@
 
 package pit
 
-//PasswordCredential is a struct for holding username/password credentials
+// PasswordCredential is a struct for holding username/password credentials
 type PasswordCredential struct {
 	Username   string `form:"username" json:"username"`
 	Password   string `form:"password" json:"password"`
