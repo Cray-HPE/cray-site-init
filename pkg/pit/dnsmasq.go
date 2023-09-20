368ba73d9256dc7535d0468a6950d66ef89be2b4
Russell Bunch <doomslayer@hpe.com>
2023-09-20 10:01:58 -0500

    MTL-2000 `packages.local` cname (#339)
    Adds `packages.local` as a CNAME to `packages.nmn`. This will trump
    `statics.conf`, causing `packages.local` to resolve to the PIT nexus
    while DNSMasq is running.
    
    This is compatible with the MTL-2000 cloud-init code, this new CNAME
    will allow `packages.local` to correctly resolve to PIT nexus during
    bootstrap. Then, during the csm install, when DNSMasq is shutdown
    `packages.local` will begin resolving to k8s nexus.

--- pkg/pit/dnsmasq.go
+++ pkg/pit/dnsmasq.go
@@ -110,6 +110,7 @@ interface-name=pit.nmn,bond0.nmn0
 domain=nmn,{{.CIDR.IP}},{{.DHCPEnd}},local
 dhcp-option=interface:bond0.nmn0,option:domain-search,nmn
 interface=bond0.nmn0
+cname=packages.local,pit.nmn
 cname=packages.nmn,pit.nmn
 cname=registry.nmn,pit.nmn
 # This needs to point to the liveCD IP for provisioning in bare-metal environments.
