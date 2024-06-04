bc76084750ee6ccc9de2604f3b86f788e8ca0f44
Russell Bunch <doomslayer@hpe.com>
2024-06-04 11:09:05 -0500

    Fix double-spaces
    Gen-Y and younger don't use double-spaces anymore, hehehe

--- scripts/gen_changelog.sh
+++ scripts/gen_changelog.sh
@@ -22,7 +22,7 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 # Run this script like so: ./genchange_log.sh <current_tag>
 
-# default is previous tag, treating tags as version numbers.  Ignore pre-release tags.
+# default is previous tag, treating tags as version numbers. Ignore pre-release tags.
 PREV_RELEASE=${1:-"$(git tag -l --sort=-v:refname | grep -v "\-" | head -1)"}
 
 THIS_VER=${2:-"HEAD"}
